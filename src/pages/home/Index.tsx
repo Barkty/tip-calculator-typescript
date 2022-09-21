0d56de2959a939b03cf261ff0c697d33e03d3408
Gbemisola Kotoye <barkty@yahoo.com>
2022-09-21 23:01:14 +0100

    tip calculator app

---
+++ src/pages/home/Index.tsx
@@ -0,0 +1,17 @@
+import { Container, Row } from 'react-bootstrap'
+import './Index.css'
+
+const Index = () => {
+
+  return (
+    <Container>
+        <h2 className='title'>spli<br/>tter</h2>
+
+        <Row>
+            
+        </Row>
+    </Container>
+  )
+}
+
+export default Index
