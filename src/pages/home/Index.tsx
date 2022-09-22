76eae8d0bbfa55e73d51401227f5f26a4c878b7c
Gbemisola Kotoye <barkty@yahoo.com>
2022-09-22 08:35:38 +0100

    tip

--- src/pages/home/Index.tsx
+++ src/pages/home/Index.tsx
@@ -1,4 +1,4 @@
-import { Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import './Index.css'
 
 const Index = () => {
@@ -8,7 +8,18 @@ const Index = () => {
         <h2 className='title'>spli<br/>tter</h2>
 
         <Row>
-            
+            <Col md={6}>
+              <div className='form_wrap'>
+                  <div className='form_group'>
+
+                  </div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className='calc_dark'>
+
+              </div>
+            </Col>
         </Row>
     </Container>
   )
